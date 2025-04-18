f4c15ff94776cda69194db1fbaf9a26c07f8236f
Paul-Holiday <pauljuststudent@gmail.com>
2025-04-19 00:52:45 +0800

    Сделал первую структуру и функцию, наладил взаимодействие с помощью указателя на структуру

--- Pointers1/main.go
+++ Pointers1/main.go
@@ -2,11 +2,29 @@ package main
 
 import "fmt"
 
+type Car struct {
+	Name  string
+	Cost  int
+	Speed float64
+}
+
+func (s *Car) Acceleration() {
+	s.Speed += 0.1
+}
+
 func main() {
-	i := 10
-	var p *int
-	p = &i
-	fmt.Println(*p)
-	*p = 20
-	fmt.Println(i)
+	// i := 10
+	//var p *int
+	// var p *int = &i
+	// fmt.Println(*p)
+	// *p = 20
+	// fmt.Println(i)
+
+	Struct1 := Car{
+		Name:  "Ford Focus",
+		Cost:  4000,
+		Speed: 60.5}
+	Struct1.Acceleration()
+
+	fmt.Println(Struct1.Speed)
 }
