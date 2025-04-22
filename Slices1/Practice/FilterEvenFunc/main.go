e2e489bda66a51ff71bc9970ddf52d4dec4c8a98
Paul-Holiday <pauljuststudent@gmail.com>
2025-04-22 22:07:43 +0800

    Я написал рабочий и эффективный код, но не соответствующий условию задачи

--- Slices1/Practice/FilterEvenFunc/main.go
+++ Slices1/Practice/FilterEvenFunc/main.go
@@ -4,23 +4,24 @@ import "fmt"
 
 func main() {
 	slice := []int{1, 2, 3, 4, 5, 6, 6, 7, 0, 8}
-	EvenedSlice := FilterEven(slice)
-	fmt.Println(EvenedSlice)
+	slice = FilterEven(slice)
+	fmt.Println(slice)
 }
 
 func FilterEven(slice []int) []int {
-	result := make([]int, 0, len(slice))
+	count := 0
 	for _, val := range slice {
 		if val%2 == 0 {
-			result = append(result, val)
+			slice[count] = val
+			count++
 		}
 	}
-	return result
+	return slice[:count]
 }
 
 // мой код рабочий и эффективный для работы со слайсом "на месте", но не соответствует условию задачи, потому что требовалось создать новый слайс, а я внёс изменения в исходный, что скорее всего породит баги, если с этим слайсом параллельно работает что-то ещё
 
-/*
+/**
 func FilterEven(slice []int) []int {
     result := make([]int, 0, len(slice)) // Заранее выделяем память
     for _, val := range slice {
