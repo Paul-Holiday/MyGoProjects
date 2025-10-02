25f26d1bf834f6c3962584e97851bf3524a87a26
Paul-Holiday <pauljuststudent@gmail.com>
2025-10-02 12:43:05 +0800

    Написал функцию определяющую, рабочий ли сегодня день

---
+++ TimePackage/IsWorkingDayFunc/go.mod
@@ -0,0 +1,3 @@
+module github.com/paul-holiday/mygoprojects/timepackage/IsWorkingDayFunc.git
+
+go 1.24.2
---
+++ TimePackage/IsWorkingDayFunc/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	now := time.Now()
+	if IsWorkingDayFunc(now) {
+		fmt.Printf("%s is a working day.\n", now.Weekday())
+	} else {
+		fmt.Printf("%s is a day off.\n", now.Weekday())
+	}
+}
+
+func IsWorkingDayFunc(day time.Time) bool {
+	weekday := day.Weekday()
+	return weekday >= time.Monday && weekday <= time.Friday
+}
