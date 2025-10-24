16fe407b518f47d0c92c16e5c914984d7d09699e
Paul-Holiday <pauljuststudent@gmail.com>
2025-10-25 03:01:09 +0800

    Сделал метод String() для интерфейса Stringer пакета fmt

--- Interfaces/StringerInterface/main.go
+++ Interfaces/StringerInterface/main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+type Book struct {
+	Title  string
+	Author string
+	Year   int
+}
+
+func (b Book) String() string {
+	return fmt.Sprintf("Книга: %s / %s (%d)", b.Title, b.Author, b.Year)
+}
+
 func main() {
-	fmt.Println()
+	book1 := Book{"Алиса в стране чудес", "Кэрролл Л.", 1865}
+	fmt.Println(book1)
 }
